a793a29c7e0a56bc09966480b1a355ff596ca506
unknown <aaron.leyesa@gmail.com>
2018-08-20 20:25:12 -0700

    added mlab

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://user1:password1@ds125872.mlab.com:25872/blog-posts';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/blog-app';
 exports.PORT = process.env.PORT || 8080;
